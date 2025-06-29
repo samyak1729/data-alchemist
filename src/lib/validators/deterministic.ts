ae3d6bd6e6a8debfa04363f71ef61fafb831d582
samyak1729 <samyak5jan@gmail.com>
2025-06-30 01:18:33 +0530

    fixing build errors pt14

--- src/lib/validators/deterministic.ts
+++ src/lib/validators/deterministic.ts
@@ -106,13 +106,12 @@ const validateOutOfRange = (data: Record<string, string | number | boolean | obj
 // Rule: Broken JSON
 const validateBrokenJson = (data: Record<string, string | number | boolean | object>[], header: string): ValidationResult[] => {
   const errors: ValidationResult[] = [];
-  data.forEach((row, rowIndex) => {
+  data.forEach((row, _rowIndex) => {
     const value = row[header];
     if (value && typeof value === 'string') {
       try {
         JSON.parse(value);
       } catch {};
-    }
   });
   return errors;
 };
